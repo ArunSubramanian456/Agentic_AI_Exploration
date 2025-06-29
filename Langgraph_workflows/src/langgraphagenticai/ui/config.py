df92ac8c8ebee72fbae80124ba1eb020ae1afe0f
ArunSubramanian456 <7979807+ArunSubramanian456@users.noreply.github.com>
2025-06-29 13:30:22 -0500

    June 29 - Config update

--- Langgraph_workflows/src/langgraphagenticai/ui/config.py
+++ Langgraph_workflows/src/langgraphagenticai/ui/config.py
@@ -1,10 +1,10 @@
 from configparser import ConfigParser
 
 class Config:
-    def __init__(self, config_file = "src/langgraphagenticai/ui/uiconfigfile.ini"):
+    def __init__(self, config_file = "./src/langgraphagenticai/ui/uiconfigfile.ini"):
         self.config = ConfigParser()
         self.config.read(config_file)
-
+    
     def get_llm_option(self):
         return self.config["DEFAULT"].get("LLM_OPTIONS").split(", ")
 
--- Langgraph_workflows/src/langgraphagenticai/ui/streamlitui/loadui.py
+++ Langgraph_workflows/src/langgraphagenticai/ui/streamlitui/loadui.py
@@ -21,16 +21,20 @@ class LoadStreamlitUI:
     def load_streamlit_ui(self):
         st.set_page_config(page_title=self.config.get_page_title(), page_icon=":robot_face:", layout="wide")
         st.header(self.config.get_page_title())
-        
+        print(self.config.get_page_title())
 
         # Sidebar for user controls
         with st.sidebar:
+            
+            llm_options = self.config.get_llm_option()
+            
 
             ## LLM Selection
-            self.user_controls['selected_llm'] = st.selectbox("Select a LLM", self.config.get_llm_option())
+            self.user_controls['selected_llm'] = st.selectbox("Select a LLM",llm_options )
 
             if self.user_controls['selected_llm'] == 'Groq':
-                self.user_controls['selected_groq_model'] = st.selectbox("Select a Model", self.config.get_groq_model_options())
+                model_options = self.config.get_groq_model_options()
+                self.user_controls['selected_groq_model'] = st.selectbox("Select a Model", model_options)
                 self.user_controls["GROQ_API_KEY"] = st.session_state["GROQ_API_KEY"] = st.text_input("Enter Groq API Key", type="password")
 
                 # Validate Groq API Key
@@ -38,7 +42,8 @@ class LoadStreamlitUI:
                     st.warning("Please enter your Groq API Key to proceed. Don't have one? refer : https://console.groq.com/keys")
 
             if self.user_controls['selected_llm'] == 'OpenAI':
-                self.user_controls['selected_openai_model'] = st.selectbox("Select a Model", self.config.get_openai_model_options())
+                model_options = self.config.get_openai_model_options()
+                self.user_controls['selected_openai_model'] = st.selectbox("Select a Model", model_options)
                 self.user_controls["OPENAI_API_KEY"] = st.session_state["OPENAI_API_KEY"] = st.text_input("Enter OpenAI API Key", type="password")
 
                 # Validate OpenAI API Key
